c5c4b2232ca4d3a00658a19fe36f69f0967e9c89
rhie-coder <quotia72@naver.com>
2024-07-15 00:41:31 +0900

    [simple-express-server] echo server

--- simple-express-server/src/index.ts
+++ simple-express-server/src/index.ts
@@ -1,1 +1,12 @@
-console.log('hello world')
+import Express, {Request, Response} from 'express';
+
+const app = Express()
+const port = 3000
+
+app.get('/', (_:Request, res:Response) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
