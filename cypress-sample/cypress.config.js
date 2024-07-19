b93fb4d6e0c4480d4473a78d4853adc536798859
rhie-coder <quotia72@naver.com>
2024-07-19 12:14:31 +0900

    [cypress-sample] cypress reports is added

--- cypress-sample/cypress.config.js
+++ cypress-sample/cypress.config.js
@@ -2,6 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    // baseUrl:"http://localhost:3000",
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
@@ -13,4 +14,12 @@ module.exports = defineConfig({
       bundler: "vite",
     },
   },
+
+  reporter: "mochawesome",
+  reporterOptions: {
+    reportDir: 'cypress/reports',
+    overwrite: false,
+    html: false,
+    json: true
+  }
 });
---
+++ cypress-sample/cypress/e2e/math.cy.js
@@ -0,0 +1,35 @@
+function add(a, b) {
+    return a + b
+  }
+  
+  function subtract(a, b) {
+    return a - b
+  }
+  
+  function divide(a, b) {
+    return a / b
+  }
+  
+  function multiply(a, b) {
+    return a * b
+  }
+  
+  describe('Unit test our math functions', () => {
+    context('math', () => {
+      it('can add numbers', () => {
+        expect(add(1, 2)).to.eq(3)
+      })
+  
+      it('can subtract numbers', () => {
+        expect(subtract(5, 12)).to.eq(-7)
+      })
+  
+      specify('can divide numbers', () => {
+        expect(divide(27, 9)).to.eq(3)
+      })
+  
+      specify('can multiply numbers', () => {
+        expect(multiply(5, 4)).to.eq(20)
+      })
+    })
+  })
