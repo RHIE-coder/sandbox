b5fa2d917f879d42e0c30cbb49eab9259275422a
rhie-coder <quotia72@naver.com>
2024-08-06 10:37:39 +0900

    [image-resizer] new

---
+++ image-resizer/resizer.py
@@ -0,0 +1,16 @@
+from PIL import Image
+
+target_name = 'imghigh.jpg'
+size = 1200
+
+def resize_image(input_path, output_path, size):
+    with Image.open(input_path) as img:
+        resized_img = img.resize(size)
+        resized_img.save(output_path)
+        print(f"이미지 크기 조절 완료: {output_path}")
+
+input_image_path = f'./images/source/{target_name}'
+output_image_path = f'./images/destination/{size}x{size}.jpg'
+new_size=(size, size)
+
+resize_image(input_image_path, output_image_path, new_size)
