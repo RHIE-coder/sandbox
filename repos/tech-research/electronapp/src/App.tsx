225850072037e9446bd6e2191dbb48fcb6f69067
rhie-coder <quotia72@naver.com>
2025-06-10 17:34:55 +0900

    [study] table lib list research

--- repos/tech-research/electronapp/src/App.tsx
+++ repos/tech-research/electronapp/src/App.tsx
@@ -1,15 +1,9 @@
-import Sample from "@/components/Sample"
+import Sample from "@/components/Sample";
  
 function App() {
 
-  const versions = window.electron.doThing()
-
-
-
   return (
     <>
-      <h1>{ctxmsg}</h1>
-      <h1>{versions.randomGenerator()}</h1>
       <Sample />
     </>
   )
--- repos/tech-research/electronapp/src/components/Sample.tsx
+++ repos/tech-research/electronapp/src/components/Sample.tsx
@@ -1,27 +1,46 @@
-import { HotTable } from "@handsontable/react-wrapper";
-import { registerAllModules } from "handsontable/registry";
-import "handsontable/styles/handsontable.css";
-import "handsontable/styles/ht-theme-main.css";
-import { useStore } from "@/store/useStore";
+// import { useStore } from "@/store/useStore";
+import { AllCommunityModule, ModuleRegistry, ColDef } from 'ag-grid-community'; 
+import { AgGridReact } from "ag-grid-react";
+import { useState } from "react";
 
-// register Handsontable's modules
-registerAllModules();
+// Register all Community features
+ModuleRegistry.registerModules([AllCommunityModule]);
+
+type CarData = {
+  make: string;
+  model: string;
+  price: number;
+  electric: boolean;
+}
 
 export default function Sample() {
-  const data = useStore((state) => state.data);
-  const headers = data[0].map(String);
-  const tableData = data.slice(1);
-  console.log(tableData);
+    // Row Data: The data to be displayed.
+    const [rowData, setRowData] = useState<CarData[]>([
+      { make: "Tesla", model: "Model Y", price: 64950, electric: true },
+      { make: "Ford", model: "F-Series", price: 33850, electric: false },
+      { make: "Toyota", model: "Corolla", price: 29600, electric: false },
+  ]);
 
+  // Column Definitions: Defines the columns to be displayed.
+  const [colDefs, setColDefs] = useState<ColDef<CarData>[]>([
+      { field: "make" },
+      { field: "model" },
+      { field: "price" },
+      { field: "electric" }
+  ]);
   return (
-    <HotTable
-      data={tableData}
-      rowHeaders={false}
-      colHeaders={headers}
-      height="auto"
-      autoWrapRow={true}
-      autoWrapCol={true}
-      licenseKey="non-commercial-and-evaluation" // for non-commercial use only
-    />
-  );
+    // Data Grid will fill the size of the parent container
+    <div style={{ height: 500 }} className="ag-theme-alpine">
+      <AgGridReact
+        rowData={rowData}
+        columnDefs={colDefs}
+      />
+    </div>
+)
 }
+
+// ✅ 1. React Spreadsheet Grid
+// ✅ 2. React-DataSheet-Grid
+// ✅ 3. Glide Data Grid
+// ✅ 4. TanStack Table + Editable Cells
+// ✅ 5. AG Grid (Community Edition)
