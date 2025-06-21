04d635741bb0a272e221362f9ce7ef81042bd01c
rhie-coder <quotia72@naver.com>
2025-06-21 17:05:08 +0900

    [study] electron

--- repos/tech-research/electronapp/src/App.tsx
+++ repos/tech-research/electronapp/src/App.tsx
@@ -2,14 +2,9 @@ import Sample from "@/components/Sample"
  
 function App() {
 
-  const versions = window.electron.doThing()
-
-
 
   return (
     <>
-      <h1>{ctxmsg}</h1>
-      <h1>{versions.randomGenerator()}</h1>
       <Sample />
     </>
   )
